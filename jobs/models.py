ad92b5c7c1248f59fc53842357ceadb2893bcb97
DgrinderHZ <hassanzekkouri@gmail.com>
2020-12-29 09:25:04 +0000

    Add Extra fields

--- jobs/models.py
+++ jobs/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 
 class Job(models.Model):
+    name = models.CharField(max_length=60)
     image = models.ImageField(upload_to='images')
     summary = models.CharField(max_length=256)
+    github = models.CharField(max_length=256)
+    demo = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.summary[0:100]
+        return self.name
